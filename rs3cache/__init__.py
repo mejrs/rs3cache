c452a7a69487824963f55744d9bc15484c45feb4
mejrs <brunokolenbrander@hotmail.com>
2021-04-17 21:17:25 +0200

    deleted definitions class

--- rs3cache/__init__.py
+++ rs3cache/__init__.py
@@ -1,18 +1,1 @@
 from rs3cache.rs3cache import *
-import functools
-
-class Definitions:
-	def __init__(self):
-		pass
-
-	@functools.cached_property
-	def location_configs(self):
-		return get_location_configs()
-
-	@functools.cached_property
-	def npc_configs(self):
-		return get_npc_configs()
-
-	@functools.cached_property
-	def varbit_configs(self):
-		return get_varbit_configs()
