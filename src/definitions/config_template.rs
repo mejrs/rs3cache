25144b988044ec5a96a8a4ef88304b12255b41c2
mejrs <brunokolenbrander@hotmail.com>
2021-11-06 16:21:31 +0100

    Add templates

---
+++ src/definitions/config_template.rs
@@ -0,0 +1,95 @@
+// A template for adding new configs
+
+use std::{
+    collections::BTreeMap,
+    fs::{self, File},
+    io::Write,
+};
+
+use path_macro::path;
+#[cfg(feature = "pyo3")]
+use pyo3::prelude::*;
+use serde::Serialize;
+use serde_with::skip_serializing_none;
+
+use crate::cache::{
+    buf::Buffer,
+    error::CacheResult,
+    index::CacheIndex,
+    indextype::{ConfigType, IndexType},
+};
+
+#[cfg_attr(feature = "pyo3", pyclass)]
+#[skip_serializing_none]
+#[derive(Debug, Default, Copy, Clone, Serialize)]
+
+pub struct <Name> {
+    /// Id of the <Name>> configuration.
+    pub id: u32,
+    
+
+
+}
+
+impl <Name> {
+    /// Returns a mapping of all [`<Name>`] configurations.
+    pub fn dump_all(config: &crate::cli::Config) -> CacheResult<BTreeMap<u32, <Name>>> {
+        Ok(CacheIndex::new(IndexType::CONFIG, &config.input)?
+            .archive(ConfigType::<Name>)?
+            .take_files()
+            .into_iter()
+            .map(|(file_id, file)| (file_id, <Name>::deserialize(file_id, file)))
+            .collect())
+    }
+
+    fn deserialize(id: u32, file: Vec<u8>) -> <Name> {
+        let mut buffer = Buffer::new(file);
+        let mut <Name> = <Name> { id, ..Default::default() };
+
+        loop {
+            let opcode = buffer.read_unsigned_byte();
+            match opcode {
+                0 => {
+                    assert_eq!(buffer.remaining(), 0);
+                    break <Name>;
+                }
+                1 => <Name>.colour = Some(buffer.read_rgb()),
+                #[cfg(feature = "rs3")]
+                2 => <Name>.op_2 = Some(buffer.read_unsigned_short()),
+                #[cfg(feature = "rs3")]
+                3 => <Name>.op_3 = Some(buffer.read_unsigned_short()),
+                #[cfg(feature = "rs3")]
+                4 => <Name>.op_4 = Some(true),
+                #[cfg(feature = "rs3")]
+                5 => <Name>.op_5 = Some(true),
+
+                missing => unimplemented!("<Name>::deserialize cannot deserialize opcode {} in id {}", missing, id),
+            }
+        }
+    }
+}
+
+#[cfg(feature = "pyo3")]
+#[pyproto]
+impl PyObjectProtocol for <Name> {
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("<Name>({})", serde_json::to_string(self).unwrap()))
+    }
+
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("<Name>({})", serde_json::to_string(self).unwrap()))
+    }
+}
+
+/// Save the <Name> configs as `location_configs.json`. Exposed as `--dump <Name>`.
+pub fn export(config: &crate::cli::Config) -> CacheResult<()> {
+    fs::create_dir_all(&config.output)?;
+    let mut <Name> = <Name>::dump_all(config)?.into_values().collect::<Vec<_>>();
+    <Name>.sort_unstable_by_key(|loc| loc.id);
+
+    let mut file = File::create(path!(config.output / "<Name>s.json"))?;
+    let data = serde_json::to_string_pretty(&<Name>).unwrap();
+    file.write_all(data.as_bytes())?;
+
+    Ok(())
+}
---
+++ src/definitions/index_template.rs
@@ -0,0 +1,101 @@
+//! Describes the properties of <Name>s.
+
+use std::{
+    collections::BTreeMap,
+    fs::{self, File},
+    io::Write,
+};
+
+use path_macro::path;
+#[cfg(feature = "pyo3")]
+use pyo3::{prelude::*, PyObjectProtocol};
+use serde::Serialize;
+
+use crate::{
+    cache::{buf::Buffer, error::CacheResult, index::CacheIndex, indextype::IndexType},
+    structures::paramtable::ParamTable,
+};
+
+/// Describes the properties of a given <Name>.
+#[cfg_eval]
+#[allow(missing_docs)]
+#[cfg_attr(feature = "pyo3", rs3cache_macros::pyo3_get_all)]
+#[cfg_attr(feature = "pyo3", pyclass)]
+#[serde_with::skip_serializing_none]
+#[derive(Serialize, Clone, Debug, Default)]
+pub struct <Name> {
+    /// Its id.
+    pub id: u32,
+   
+    #[serde(flatten)]
+    pub params: Option<ParamTable>,
+}
+
+impl <Name> {
+    /// Returns a mapping of all [`<Name>`]s.
+    pub fn dump_all(config: &crate::cli::Config) -> CacheResult<BTreeMap<u32, Self>> {
+        let archives = CacheIndex::new(IndexType::OBJ_CONFIG, &config.input)?.into_iter();
+
+        let <Name>s = archives
+            .flat_map(|archive| {
+                let archive_id = archive.archive_id();
+                archive
+                    .take_files()
+                    .into_iter()
+                    .map(move |(file_id, file)| (archive_id << 8 | file_id, file))
+            })
+            .map(|(id, file)| (id, Self::deserialize(id, file)))
+            .collect::<BTreeMap<u32, Self>>();
+        Ok(<Name>s)
+    }
+
+    fn deserialize(id: u32, file: Vec<u8>) -> Self {
+        let mut buffer = Buffer::new(file);
+        let mut <Name> = Self { id, ..Default::default() };
+
+        loop {
+            match buffer.read_unsigned_byte() {
+                0 => {
+                    debug_assert_eq!(buffer.remaining(), 0);
+                    break <Name>;
+                }
+                249 => <Name>.params = Some(ParamTable::deserialize(&mut buffer)),
+
+                missing => unimplemented!("<Name>::deserialize cannot deserialize opcode {} in id {}", missing, id),
+            }
+        }
+    }
+}
+
+use std::fmt::{self, Display, Formatter};
+
+impl Display for <Name> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", serde_json::to_string_pretty(&self).unwrap())
+    }
+}
+
+#[cfg(feature = "pyo3")]
+#[pyproto]
+impl PyObjectProtocol for <Name> {
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("<Name>({})", serde_json::to_string(self).unwrap()))
+    }
+
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("<Name>({})", serde_json::to_string(self).unwrap()))
+    }
+}
+
+/// Save the <Name> configs as `<Name>>.json`. Exposed as `--dump <Name>`.
+pub fn export(config: &crate::cli::Config) -> CacheResult<()> {
+    fs::create_dir_all(&config.output)?;
+    let mut <Name>_configs = <Name>::dump_all(config)?.into_values().collect::<Vec<_>>();
+    <Name>_configs.sort_unstable_by_key(|loc| loc.id);
+
+    let mut file = File::create(path!(config.output / "<Name>_configs.json"))?;
+    let data = serde_json::to_string_pretty(&<Name>_configs).unwrap();
+    file.write_all(data.as_bytes())?;
+
+    Ok(())
+}
