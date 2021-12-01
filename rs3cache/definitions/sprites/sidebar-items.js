51382119d9062ba5292746b2648fed250e5cffd0
mejrs <mejrs@users.noreply.github.com>
2021-12-01 09:12:43 +0000

    Upload documentation

--- rs3cache/definitions/sprites/sidebar-items.js
+++ rs3cache/definitions/sprites/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["dumps","Returns a [`BTreeMap`] holding all sprites in `ids`."],["save_all","Saves an image of every sprite to disk."]],"type":[["Sprite","Type alias for a rgba image."]]});
+initSidebarItems({"fn":[["deserialize",""],["dumps","Returns a [`BTreeMap`] holding all sprites in `ids`."],["save_all","Saves an image of every sprite to disk."]],"type":[["Sprite","Type alias for a rgba image."]]});
