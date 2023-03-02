94fa7615b850d6fac6e311b401ceae3c36b960ab
mejrs <mejrs@users.noreply.github.com>
2023-03-02 14:49:07 +0000

    Upload documentation

--- rs3cache_backend/decoder/sidebar-items.js
+++ rs3cache_backend/decoder/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["DecodeError",""]],"fn":[["decompress","Decompresses index files."]]};
+window.SIDEBAR_ITEMS = {"enum":[["DecodeError",""]],"fn":[["decompress","Decompresses index files."]],"struct":[["BZip2",""],["Empty",""],["Gzip",""],["Unimplemented",""],["Zlib",""]]};
